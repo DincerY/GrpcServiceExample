b2396bf8649876fc2740f1d05cdfa1eb347d24e1
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-02-27 18:02:19 +0300

    Add project files.

---
+++ GrpcServiceClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using Grpc.Net.Client;
+using GrpcGreeterClient;
+
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7183");
+var client = new Greeter.GreeterClient(channel);
+
+Console.WriteLine("Adınızı giriniz : ");
+var name = Console.ReadLine();
+Console.WriteLine("Soy adınızı giriniz : ");
+var surname = Console.ReadLine();
+
+var reply = await client.SayHelloAsync(
+    new HelloRequest { Name = name , Surname = surname});
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
